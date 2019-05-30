4b8a93b39712b0c2101e6933d05ad506d0fc8a14
Jake O'Donnell <jakeod99@gmail.com>
2019-05-30 13:07:42 -0400

    Now recognizes when a 'Question' is acting as a label

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,8 +1,11 @@
 module HomeHelper
-    def populateQuestionnaire
-        @questionnaire = FHIR::Questionnaire.create(
-            title: "DEL Demo Questionnaire",
-            publisher: "The MITRE Corporation",
-            experimental: true)
+  def isQuestionActuallyLabel?(assessments, listIndex)
+    id = assessments[listIndex]["Item ID"]
+    section = assessments[listIndex]["Section Name"]
+    isLabel = false
+    if !section.include?("Section J") && !section.include?("Integumentary Status")
+        isLabel = assessments.length > listIndex + 1 && assessments[listIndex+1]["Item ID"].include?(id)
     end
+    return isLabel
+  end
 end
