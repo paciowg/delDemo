3068f3bbb5c50882c8bb4d06bd0531eb357ff48a
Jake O'Donnell <jakeod99@gmail.com>
2019-05-31 16:40:44 -0400

    Now recieves all 28 questionnaires by accounting for fhir server paging

--- app/models/get_questionnaires.rb
+++ app/models/get_questionnaires.rb
@@ -8,16 +8,51 @@ class GetQuestionnaires
         questionnaires = CSV.read("storage/Data_Elements.csv")
         keys = questionnaires[0]
         questionnaires = questionnaires.drop(1)
-        return questionnaires.map{ |values| Hash[keys.zip(values)] }
+        questionnaires.map{ |values| Hash[keys.zip(values)] }
     end
 
-    def self.getAllQuestionnaires
+    def self.setClientConnection
         @url = "https://impact-fhir.mitre.org/r4/"
         @client = FHIR::Client.new(@url)
         FHIR::Model.client = @client
-        allQuestionnaires = @client.read_feed(FHIR::Questionnaire) # fetch Bundle of Questionnaires
-        json = allQuestionnaires.response.values[2]
+    end
+
+    def self.getAllQuestionnaires
+        setClientConnection()
+        initialQuestionnaires = @client.read_feed(FHIR::Questionnaire)
+        json = initialQuestionnaires.response.values[2]
         @questionnaireHash = JSON.parse(json)
+
+        hashes = [].append(@questionnaireHash)
+        i = 0
+        while true
+            shouldBreak = true
+            nextLink = ''
+            break if hashes[i]["link"].nil?
+            hashes[i]["link"].each do |link|
+                if !link["url"].nil? && link["relation"].eql?("next") && !link["url"].empty?
+                    shouldBreak = false
+                    nextLink = link["url"] + "?_format=json"
+                end
+            end
+            break if shouldBreak
+            i += 1
+            nextQuestionnaires = @client.raw_read_url(nextLink)
+            nextJSON = nextQuestionnaires.response.values[2]
+            hashes.append(JSON.parse(nextJSON))
+        end
+
+        @questionnaireHash = combineQuestionnaires(hashes)
+    end
+
+    def self.combineQuestionnaires(hashes)
+        finalEntry = Array.new
+        hashes.each do |hash|
+            return nil if hash["entry"].nil?
+            finalEntry.append(hash["entry"])
+        end
+        hashes[0]["entry"] = finalEntry.flatten
+        hashes[0]
     end
 
     def self.getQuestionnaire(version)
