cd17812bccf782e106bdac06d7226dcdeb3d1567
Jake O'Donnell <jakeod99@gmail.com>
2019-06-11 10:34:37 -0400

    Form now delivered in multiple pages

--- app/controllers/questionnaire_controller.rb
+++ app/controllers/questionnaire_controller.rb
@@ -2,6 +2,22 @@ class QuestionnaireController < ApplicationController
   def index
     @questionnaire = GetQuestionnaires.getQuestionnaire(params[:version])
     @currentSection = nil
+    if params[:page].nil?
+      @currentSection = 1
+    elsif params[:page][0..3].eql?("back")
+      @currentSection = params[:page].split(",").last.to_i
+      session.delete(@currentSection.to_s)
+    elsif params[:page].eql?("submit")
+      # submit logic (render submission page, finish )
+      session["last"] = helpers.getRelevantParams(params)
+      render 'submission'
+    else
+      @currentSection = params[:page].to_i
+      session[(@currentSection - 1).to_s] = helpers.getRelevantParams(params)
+    end
+
+    puts session
+
     if @questionnaire.nil?
       render 'questionnaire/error'
     end
--- app/helpers/questionnaire_helper.rb
+++ app/helpers/questionnaire_helper.rb
@@ -1,4 +1,24 @@
 module QuestionnaireHelper
+    def getSectionedQuestionnaire(q)
+        qFlat = flattenQuestionnaire(q)
+        qSections = []
+        lastRoot = 0
+        qFlat.each_with_index do |qItem, i|
+            if qItem.level == 0 && i != 0
+                qSections.push(qFlat[lastRoot..(i - 1)])
+                lastRoot = i
+            end
+        end
+        qSections.push(qFlat[lastRoot..qFlat.length])
+        qSections
+    end
+
+    # returns the requested page along with a number representing how many pages there are total
+    def getPage(q, page)
+        sectQ = getSectionedQuestionnaire(q)
+        [sectQ[page - 1], sectQ.length]
+    end
+    
     def flattenQuestionnaire(q)
         qFlat = [].append(getItem(q["resource"]["item"]))
         qFlat.flatten()
@@ -27,5 +47,20 @@ module QuestionnaireHelper
             options.append([concept["display"], concept["code"]])
         }
         options
+    end
+
+    def getRelevantParams(params)
+        revisedParams = Hash.new
+        avoidKeys = ["page", "utf8", "attempt"]
+        params.each_pair { |key, value|
+            if !avoidKeys.include?(key)
+                revisedParams[key] = value
+            end
+        }
+        params
+    end
+
+    def questionnaireError()
+        ApplicationController.render 'questionnaire/error'
     end
 end
