d5624bdbd60a9e73445f6ca04e27eabba589dacb
Jake O'Donnell <jakeod99@gmail.com>
2019-07-08 12:36:27 -0400

    Protects against potential memory leak

--- app/models/session_stack.rb
+++ app/models/session_stack.rb
@@ -3,7 +3,8 @@ class SessionStack
     @@sessionHash = Hash.new
 
     def self.create(id)
-        @@sessionHash[id] = []
+        @@sessionHash[id] = [{"started" => Time.now}]
+        prune()
     end
 
     def self.read(id)
@@ -12,23 +13,19 @@ class SessionStack
 
     def self.push(id, input)
         @@sessionHash[id].each do |section|
-            if section.keys.include?(input.keys[1]) #banks on there being an initial "version" key-value pair
+            if section.keys.include?(input.keys[1]) #banks on input having an initial "version" key-value pair
                 @@sessionHash[id].delete(section)
             end
         end
         @@sessionHash[id].push(input)
     end
 
-    def self.pop(id)
-        @@sessionHash[id].pop()
-    end
-
     def self.delete(id)
         @@sessionHash.delete(id)
     end
 
-    def self.delete_all()
-        @@sessionHash.clear()
+    def self.prune() #removes sessions older than 3 hours
+        @@sessionHash.delete_if { |id, session| (Time.now - session[0]["started"]) > (3 * 60 * 60) }
     end
 
 end
