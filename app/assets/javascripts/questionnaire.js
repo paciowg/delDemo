4419dc37fcd8cf02da8ab571a78962623501ca74
Jake O'Donnell <jakeod99@gmail.com>
2019-07-09 14:47:42 -0400

    Open-choice date ready (as it can be) for server updates

--- app/assets/javascripts/questionnaire.js
+++ app/assets/javascripts/questionnaire.js
@@ -13,8 +13,36 @@
 
     };
 
+    window.toggle = {
+
+        dateShow: function() {
+            $(".date-collapse").on("show.bs.collapse", function() {
+                $(".date-collapse.in input.month").attr("pattern", "0*([1-9]|1[0-2])")
+                $(".date-collapse.in input.month").attr("pattern", "0*([1-9]|[1-2][0-9]|3[0-1])")
+                $(".date-collapse.in input.month").attr("pattern", "0*(19[0-9][0-9]|20[0-1][0-9])")
+            })
+        },
+
+        dateHide: function() {
+            $(".date-collapse").on("hide.bs.collapse", function() {
+                $(".date-collapse.in input.month").attr("pattern", "")
+                $(".date-collapse.in input.month").attr("pattern", "")
+                $(".date-collapse.in input.month").attr("pattern", "")
+            })
+        }
+
+    }
+
 })(jQuery)
 
 $(document).ready(window.disable.formEnter);
 
 $(document).on('turbolinks:load', window.disable.formEnter)
+
+$(document).ready(window.toggle.dateShow);
+
+$(document).on('turbolinks:load', window.toggle.dateShow)
+
+$(document).ready(window.toggle.dateHide);
+
+$(document).on('turbolinks:load', window.toggle.dateHide)
