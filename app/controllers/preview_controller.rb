f922b032bc1078bca132dd28f145485378a5a634
Jake O'Donnell <jakeod99@gmail.com>
2019-07-31 13:15:38 -0400

    generated preview

---
+++ app/controllers/preview_controller.rb
@@ -0,0 +1,5 @@
+class PreviewController < ApplicationController
+  def index
+    
+  end
+end
--- app/controllers/questionnaire_controller.rb
+++ app/controllers/questionnaire_controller.rb
@@ -14,7 +14,7 @@ class QuestionnaireController < ApplicationController
     elsif params[:page].eql?("submit")
       # submit logic (render submission page, finish )
       SessionStack.push(session.id, helpers.getRelevantParams(params))
-      redirect_to url_for(controller: "submission", action: "index")
+      redirect_to url_for(controller: "preview", action: "index")
 
     else
       @currentSection = params[:page].to_i
