42087dfa90b22c4bec989a162897790e32e0e9a2
Jake O'Donnell <jakeod99@gmail.com>
2019-08-06 14:31:25 -0400

    preview page now reached with partial qr

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -1,5 +1,9 @@
 class PreviewController < ApplicationController
   def index
-    
+    @questionnaire = GetQuestionnaires.getQuestionnaire(SessionStack.read(session.id)[1]["version"])
+
+    @tempForPreviewDev = SessionStack.read(session.id)
+
+    @assessment = helpers.constructAssessment(session.id, @questionnaire)
   end
 end
--- app/helpers/preview_helper.rb
+++ app/helpers/preview_helper.rb
@@ -1,2 +1,64 @@
 module PreviewHelper
+
+    def constructAssessment(sessionID, quest)
+        qSects = getSectionedQuestionnaire(quest)
+        currentLevel = 0
+        sesh = SessionStack.read(sessionID)
+
+        assessment = FHIR::QuestionnaireResponse.new
+        assessment.status = "in-progress"
+        assessment.questionnaire = quest.url
+        assessment.subject = FHIR::Reference.new
+        assessment.subject.type = "Questionnaire"
+        timeStr = Time.now.to_s.gsub(" ", "");
+        assessment.authored = timeStr[0..9] + "T" + timeStr[10..-3] + ":" + timeStr[-2..-1]
+
+        qSects.each_with_index do |qSect, page|
+            assessment.item.push(FHIR::QuestionnaireResponse::Item.new)
+            assessment.item[-1] = qrPopulateItem(assessment.item.last, sesh, qSect, page + 1)
+        end
+
+        assessment
+    end
+
+    def qrPopulateItem(item, sesh, qSect, page, index = 0)
+        item.text = qSect[index].item.text
+        item.linkId = qSect[index].item.linkId
+
+        itemSeshKeys = sesh[page].keys.select{ |key| key.include?(qSect[index].item.linkId) }
+        if itemSeshKeys.length > 0 && !qSect[index].item.type.end_with?("display", "group")
+            answers = itemSeshKeys.collect{ |key| sesh[page][key] }
+            answers.each do |answer|
+                if qSect[index].item.type.eql?("integer")
+                    item.answer.push(FHIR::QuestionnaireResponse::Item::Answer.new({valueInteger: answer.to_i}))
+                elsif qSect[index].item.type.eql?("decimal")
+                    item.answer.push(FHIR::QuestionnaireResponse::Item::Answer.new({valueDecimal: answer.to_f}))
+                else
+                    if qSect[index].item.answerOption
+                        answerCoding = qSect[page].item.answerOption.find{ |code| code.valueCoding.code.eql?(answer) }
+                        if answerCoding
+                            item.answer.push(answerCoding)
+                        else
+                            item.answer.push(FHIR::QuestionnaireResponse::Item::Answer.new({valueString: answer}))
+                        end
+                    else
+                        item.answer.push(FHIR::QuestionnaireResponse::Item::Answer.new({valueString: answer}))
+                    end
+                end
+            end
+        end
+
+        newIndex = index + 1
+        currentLevel = qSect[index].level
+        while qSect[newIndex] && qSect[newIndex].level > currentLevel
+            if qSect[newIndex].level == currentLevel + 1
+                item.item.push(FHIR::QuestionnaireResponse::Item.new)
+                qrPopulateItem(item.item.last, sesh, qSect, page, newIndex)
+            end
+            newIndex += 1
+        end
+
+        item
+    end
+
 end
