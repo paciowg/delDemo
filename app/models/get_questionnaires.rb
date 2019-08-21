66b4be8d2755ead0be80600097f95ccbb9633b44
Jake O'Donnell <jakeod99@gmail.com>
2019-08-21 12:44:25 -0400

    Fixed empty questionnaire error

--- app/models/get_questionnaires.rb
+++ app/models/get_questionnaires.rb
@@ -3,7 +3,7 @@ require 'json'
 class GetQuestionnaires
 
     def self.setConnection
-        @url = "https://api.logicahealth.org/PACIO/open"    
+        @url = "https://api.logicahealth.org/PACIO/open"
         @client = FHIR::Client.new(@url)
         FHIR::Model.client = @client
     end
