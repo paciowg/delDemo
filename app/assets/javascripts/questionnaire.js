b52a614fd80eeb905c42f925b3b706143e18a565
Jake O'Donnell <jakeod99@gmail.com>
2019-08-30 15:08:44 -0400

    functional about link

--- app/assets/javascripts/questionnaire.js
+++ app/assets/javascripts/questionnaire.js
@@ -18,7 +18,7 @@
         dateShow: function() {
             $(".date-collapse.show input.month").attr("pattern", "0*([1-9]|1[0-2])")
             $(".date-collapse.show input.day").attr("pattern", "0*([1-9]|[1-2][0-9]|3[0-1])")
-            $(".date-collapse.show input.year").attr("pattern", "0*(19[0-9][0-9]|20[0-1][0-9])")
+            $(".date-collapse.show input.year").attr("pattern", "0*(19[0-9][0-9]|20[0-1][0-9]|2020)")
             $(".date-collapse.show input.month").attr("required", true)
             $(".date-collapse.show input.day").attr("required", true)
             $(".date-collapse.show input.year").attr("required", true)
@@ -44,6 +44,15 @@
         dateHideListener: function() {
             window.toggle.dateHide()
             $(".date-collapse").on("hidden.bs.collapse", window.toggle.dateHide)
+        },
+
+        modalLinkListener: function() {
+            $("a.about").click(function(){
+                $(".modal-footer > a").attr("href", "/about")
+            });
+            $(".banner-content").click(function(){
+                $(".modal-footer > a").attr("href", "/")
+            });
         }
 
     }
@@ -51,10 +60,13 @@
 })(jQuery)
 
 $(document).ready(window.disable.formEnter);
-$(document).on('turbolinks:load', window.disable.formEnter)
+$(document).on('turbolinks:load', window.disable.formEnter);
 
 $(document).ready(window.toggle.dateShowListener);
-$(document).on('turbolinks:load', window.toggle.dateShowListener)
+$(document).on('turbolinks:load', window.toggle.dateShowListener);
 
 $(document).ready(window.toggle.dateHideListener);
-$(document).on('turbolinks:load', window.toggle.dateHideListener)
+$(document).on('turbolinks:load', window.toggle.dateHideListener);
+
+$(document).ready(window.toggle.modalLinkListener);
+$(document).on('turbolinks:load', window.toggle.modalLinkListener);
--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,5 @@
 class AboutController < ApplicationController
     def index
+        SessionStack.delete(session.id)
     end
 end
