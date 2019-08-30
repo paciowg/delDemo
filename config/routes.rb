adaf362cde728feb1d181df2b7f36d862441d5e7
Jake O'Donnell <jakeod99@gmail.com>
2019-08-30 13:55:17 -0400

    generated about controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/error', to: 'home#error'
   get '/read_error', to: 'home#read_error'
+  get '/about', to: 'about#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
