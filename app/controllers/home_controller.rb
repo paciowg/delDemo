901364278b4046fbc7961c0e5bd1e48f9ac06ee2
Jake O'Donnell <jakeod99@gmail.com>
2019-09-27 09:35:59 -0400

    inline search on landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
-  def index
-    @questionnaireVersions = GetQuestionnaires.getQuestionnaireVersions()
-
-    SessionStack.delete(session.id)
-  end
+    def index
+        questionnaireVersions = GetQuestionnaires.getQuestionnaireVersions()
+        @activeVersions = Array.new
+        @inactiveVersions = Array.new
+        questionnaireVersions.each do |qv|
+            qv[:status].eql?("active") ? @activeVersions.append(qv) : @inactiveVersions.append(qv)
+        end
+        SessionStack.delete(session.id)
+    end
 end
