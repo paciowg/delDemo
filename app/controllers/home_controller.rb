9650a2dd2b35b5ddaabafdfd0a6033f2ba76c959
Jake O'Donnell <jakeod99@gmail.com>
2019-10-02 16:11:00 -0400

    replaced library refs with questionnaire

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,6 @@ class HomeController < ApplicationController
             qv[:status].eql?("active") ? @qssActive.append(qv) : @qssInactive.append(qv)
         end
 
-        @lssActive = Array.new
-        @lssInactive = Array.new
-        ServerInteraction.getSummaries(FHIR::Library).each do |qv|
-            qv[:status].eql?("active") ? @lssActive.append(qv) : @lssInactive.append(qv)
-        end
-
         SessionStack.delete(session.id)
     end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,11 @@ class SearchController < ApplicationController
         @input = params[:input]
         unfilteredQs = ServerInteraction.search(FHIR::Questionnaire, @input)
         @qs = helpers.searchItems(unfilteredQs, @input)
+
+        @qssActive = Array.new
+        @qssInactive = Array.new
+        ServerInteraction.getSummaries(FHIR::Questionnaire).each do |qv|
+            qv[:status].eql?("active") ? @qssActive.append(qv) : @qssInactive.append(qv)
+        end
     end
 end
