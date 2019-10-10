3130833dd25cbd895dba95194bafe2ee557f6f2e
Jake O'Donnell <jakeod99@gmail.com>
2019-10-10 16:17:57 -0400

    Specific item detail view

--- app/controllers/detail_controller.rb
+++ app/controllers/detail_controller.rb
@@ -3,8 +3,8 @@ class DetailController < ApplicationController
         serverInteraction = ConnectionTracker.get(session.id)
 
         @input = params[:input]
-        @assessment = params[:assessment]
-        questionnaire = serverInteraction.getSpecificResource(FHIR::Questionnaire, @assessment)
+        @assessmentID = params[:assessment]
+        questionnaire = serverInteraction.getSpecificResource(FHIR::Questionnaire, @assessmentID)
         @item = helpers.getItemByID(questionnaire, params[:id])
 
         @qssActive = Array.new
@@ -12,5 +12,7 @@ class DetailController < ApplicationController
         serverInteraction.getSummaries(FHIR::Questionnaire).each do |qv|
             qv[:status].eql?("active") ? @qssActive.append(qv) : @qssInactive.append(qv)
         end
+
+        @assessmentName = (@qssActive + @qssInactive).select{ |qs| qs[:id].eql?(@assessmentID) }.first[:name]
     end
 end
